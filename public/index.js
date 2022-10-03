c3e5d7e882a028384ed8d7afa27f4143345cf392
LeL-nardo <sandroleonardoads@gmail.com>
2022-10-03 19:15:18 -0300

    rgb v1.0

--- public/index.js
+++ public/index.js
@@ -2,12 +2,20 @@ let id = 0
 const q0 = document.getElementById("q0")
 const q1 = document.getElementById("q1")
 const q2 = document.getElementById("q2")
+const botao = document.getElementsByTagName("button")
+const recado = document.getElementById("recado")
 
 let rgb = [0, 0, 0]
 
 function gerarCor() {
     id = Math.floor(Math.random() * 3)
     console.log(id)
+    recado.innerHTML = "Adivinhe a Cor..."
+
+    /*Desativando botão para evitar erros*/
+    botao[0].setAttribute('disabled','disabled')
+    botao[0].setAttribute("class", "desativado")
+
     /*Variaveis para o texto*/
     const rtxt = document.getElementById("r")
     const gtxt = document.getElementById("g")
@@ -20,9 +28,9 @@ function gerarCor() {
         if (c == 0) {
             q0.style.background = `rgb(${r}, ${g}, ${b})`
             if (id == 0) {
-                rtxt.innerHTML = r
-                gtxt.innerHTML = g
-                btxt.innerHTML = b
+                rtxt.innerHTML = `${r}%`
+                gtxt.innerHTML = `${g}%`
+                btxt.innerHTML = `${b}%`
 
                 q0.addEventListener("click", ganhou)
                 q1.addEventListener("click", perdeu)
@@ -34,9 +42,9 @@ function gerarCor() {
         } else if (c == 1) {
             q1.style.background = `rgb(${r}, ${g}, ${b})`
             if (id == 1) {
-                rtxt.innerHTML = r
-                gtxt.innerHTML = g
-                btxt.innerHTML = b
+                rtxt.innerHTML = `${r}%`
+                gtxt.innerHTML = `${g}%`
+                btxt.innerHTML = `${b}%`
 
                 q1.addEventListener("click", ganhou)
                 q0.addEventListener("click", perdeu)
@@ -48,9 +56,9 @@ function gerarCor() {
         } else {
             q2.style.background = `rgb(${r}, ${g}, ${b})`
             if (id == 2) {
-                rtxt.innerHTML = r
-                gtxt.innerHTML = g
-                btxt.innerHTML = b
+                rtxt.innerHTML = `${r}%`
+                gtxt.innerHTML = `${g}%`
+                btxt.innerHTML = `${b}%`
 
                 q2.addEventListener("click", ganhou)
                 q1.addEventListener("click", perdeu)
@@ -60,10 +68,17 @@ function gerarCor() {
                 rgb.splice(2, 1, b)
             }
         }
-        console.log(rgb)
     }
 }
 function ganhou() {
+    recado.innerHTML = "Acertou!!"
+    gerenciando()
+}
+function perdeu() {
+    recado.innerHTML = "Errou..."
+    gerenciando()
+}
+function gerenciando() {
     if (id == 0) {
         /*Retirando eventos e trocando cores*/
         q0.removeEventListener("click", ganhou)
@@ -89,8 +104,8 @@ function ganhou() {
         q1.style.background = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
         q0.style.background = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
     }
-}
-function perdeu() {
-    alert("perdeu")
+        /*Ativando o botão*/
+        botao[0].removeAttribute("disabled", "disabled")
+        botao[0].removeAttribute("class", "desativado")
 }
 gerarCor()
